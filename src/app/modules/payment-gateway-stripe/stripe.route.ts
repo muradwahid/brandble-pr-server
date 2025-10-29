be8e6da20224700b8f2871918731887e00659094
muradwahid <muradwahid334@gmail.com>
2025-10-29 21:12:20 +0600

    remove process role

--- src/app/modules/payment-gateway-stripe/stripe.route.ts
+++ src/app/modules/payment-gateway-stripe/stripe.route.ts
@@ -10,7 +10,9 @@ router.post('/create-payment-intent', auth(ENUM_USER_ROLE.CLIENT, ENUM_USER_ROLE
 router.post('/save-payment-info', auth(ENUM_USER_ROLE.CLIENT, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),StripeController.savePaymentInfo);
 router.post('/setup-intent', auth(ENUM_USER_ROLE.CLIENT, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),StripeController.createSetupIntent);
 router.post('/save-method', auth(ENUM_USER_ROLE.CLIENT, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),StripeController.savePaymentMethod);
-router.post('/process-payment', auth(ENUM_USER_ROLE.CLIENT,ENUM_USER_ROLE.ADMIN,ENUM_USER_ROLE.SUPER_ADMIN),StripeController.processPayment);
+router.post('/process-payment',
+  // auth(ENUM_USER_ROLE.CLIENT, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  StripeController.processPayment);
 router.get('/methods', auth(ENUM_USER_ROLE.CLIENT, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),StripeController.getPaymentMethods);
 router.post('/set-default', auth(ENUM_USER_ROLE.CLIENT, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),StripeController.setDefaultPaymentMethod);
 router.delete('/delete-method/:id', auth(ENUM_USER_ROLE.CLIENT, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),StripeController.deletePaymentMethod);
